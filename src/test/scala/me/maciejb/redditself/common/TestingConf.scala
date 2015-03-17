44180520f42624c958e9a7a641de235c53102888
Maciej Bilas <maciej.bilas@gmail.com>
2015-03-17 13:40:43 +0100

    Testing metrics reporting.

---
+++ src/test/scala/me/maciejb/redditself/common/TestingConf.scala
@@ -0,0 +1,14 @@
+package me.maciejb.redditself.common
+
+import com.typesafe.config.{ConfigFactory, Config}
+
+
+trait TestingConf {
+  def config: Config
+
+  def signalFxAuthToken = config.getString("metrics.signal_fx.auth_token")
+}
+
+object TestingConf extends TestingConf {
+  override lazy val config = ConfigFactory.load("testing.conf")
+}
---
+++ src/test/scala/me/maciejb/redditself/infrastructure/MetricsReportingApp.scala
@@ -0,0 +1,51 @@
+package me.maciejb.redditself.infrastructure
+
+import java.util.concurrent.TimeUnit
+
+import com.codahale.metrics.Slf4jReporter
+import com.signalfx.codahale.reporter.SignalFxReporter.Builder
+import dispatch.Defaults._
+import dispatch._
+import me.maciejb.redditself.common.TestingConf
+import me.maciejb.redditself.infrastructure.AppMetricsRegistry.metricRegistry
+import me.maciejb.redditself.redditapi.{UserCommentsClient, UserCommentsClientSpec}
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.Future
+
+/**
+ * Manual test that should report a metric.
+ */
+
+object MetricsReportingApp extends App {
+
+  val commentsClient = new UserCommentsClient(UserCommentsClientSpec.WayFairer)
+
+  def getComments = Future.sequence((0 to 10).map(_ => commentsClient.comments))
+
+  def setupMetrics = {
+    val logReporter = Slf4jReporter.forRegistry(metricRegistry)
+      .outputTo(LoggerFactory.getLogger("me.maciejb.redditself.metrics"))
+      .convertRatesTo(TimeUnit.SECONDS)
+      .convertDurationsTo(TimeUnit.MILLISECONDS)
+      .build()
+
+    val signalFxReporter = new Builder(metricRegistry, TestingConf.signalFxAuthToken).
+      setDurationUnit(TimeUnit.SECONDS).
+      setRateUnit(TimeUnit.MILLISECONDS).
+      setName("reddit_self").
+      build()
+
+    Seq(logReporter, signalFxReporter)
+  }
+
+  val metricReporters = setupMetrics
+  val futComments = getComments
+
+  futComments onComplete { _ => metricReporters.foreach { reporter =>
+    reporter.report()
+    reporter.stop()
+  }
+  }
+  futComments onComplete { _ => Http.shutdown()}
+}

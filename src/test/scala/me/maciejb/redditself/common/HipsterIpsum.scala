bf8c80f3b829ed034c75873acba4b6c1b288b6d6
Maciej Bilas <maciej.bilas@gmail.com>
2015-05-13 14:17:03 +0200

    Add missing Hipster Ipsum helper class.

---
+++ src/test/scala/me/maciejb/redditself/common/HipsterIpsum.scala
@@ -0,0 +1,37 @@
+package me.maciejb.redditself.common
+
+
+object HipsterIpsum {
+
+  val Paragraphs4 =
+    """
+      |Austin umami freegan occupy master cleanse bespoke. 8-bit Neutra lumbersexual, semiotics 3 wolf moon mlkshk
+      |pickled paleo sriracha Portland 90's occupy Odd Future. Brunch quinoa sartorial, Marfa shabby chic Vice Etsy
+      |Godard single-origin coffee four loko tofu Pinterest. 90's hashtag wayfarers sustainable, four loko pour-over
+      |High Life migas hoodie small batch Schlitz gluten-free selfies Vice. Schlitz PBR&B food truck church-key banh mi
+      |iPhone, photo booth retro 3 wolf moon gastropub hella gluten-free craft beer. Sriracha heirloom Intelligentsia
+      |McSweeney's salvia. Art party leggings mlkshk slow-carb bicycle rights, VHS Neutra small batch authentic
+      |Shoreditch next level crucifix.
+      |
+      |Neutra fashion axe Truffaut, authentic Pinterest biodiesel shabby chic freegan mumblecore Etsy. McSweeney's
+      |umami ennui messenger bag, jean shorts Austin Etsy Helvetica paleo lo-fi meditation forage Tumblr organic
+      |tattooed. IPhone synth McSweeney's messenger bag. Raw denim chia VHS, scenester butcher whatever distillery
+      |hoodie jean shorts Shoreditch. Freegan messenger bag jean shorts cardigan tilde, pickled synth pour-over craft
+      |beer narwhal ugh meggings cliche. Fap tofu hoodie Pitchfork trust fund. Sriracha hoodie drinking vinegar
+      |listicle Carles pork belly.
+      |
+      |Fingerstache drinking vinegar yr, taxidermy lomo messenger bag pug kogi mlkshk. Try-hard 8-bit salvia hella,
+      |meggings 90's actually swag tilde bicycle rights Blue Bottle authentic locavore. Truffaut gentrify retro Brooklyn.
+      |Sartorial pickled hashtag iPhone. Messenger bag cold-pressed fashion axe, put a bird on it forage keffiyeh you
+      |probably haven't heard of them twee locavore 3 wolf moon lo-fi cardigan master cleanse Banksy Godard. American
+      |Apparel meditation blog irony PBR&B Pinterest Carles High Life tousled, gentrify Intelligentsia dreamcatcher.
+      |Messenger bag VHS raw denim narwhal ugh vegan.
+      |
+      |Biodiesel actually chia street art shabby chic, pickled tote bag farm-to-table. XOXO selvage Truffaut umami
+      |banjo tousled. Gentrify fingerstache 8-bit Pitchfork flannel PBR&B tofu shabby chic. Selfies meh post-ironic,
+      |8-bit irony DIY ethical. Post-ironic you probably haven't heard of them Echo Park, fashion axe semiotics
+      |tattooed freegan brunch. Art party 90's crucifix, readymade Intelligentsia ugh beard heirloom Blue Bottle
+      |stumptown Truffaut locavore fashion axe before they sold out cred. Meh kitsch Blue Bottle, keffiyeh heirloom
+      |plaid post-ironic twee ethical leggings distillery brunch.
+    """.stripMargin
+}
